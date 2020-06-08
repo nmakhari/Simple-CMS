152cbccc441f14db6242c2186c3b4eb25ccd68f4
nmakhari <nmakhari@uwaterloo.ca>
2020-06-07 23:04:02 -0400

    Implemented Delete/Destroy functionality on Subject

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -42,9 +42,13 @@ class SubjectsController < ApplicationController
 
   #deleting
   def delete
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    redirect_to(subjects_path)
   end
 
   private
