b23912fcacac7aca43fefa39ee84022e1ce0638e
nmakhari <nmakhari@uwaterloo.ca>
2020-06-09 23:39:54 -0400

    Pages News and Create

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -31,8 +31,18 @@ class PagesController < ApplicationController
   end
 
   def create
+    @page = Page.new(page_params)
+      if @page.save
+        flash[:notice] = "Page ''#{@page.name}' successfully created"
+        redirect_to(pages_path)
+      else
+        render('new')
+      end
   end
+  private
 
-  
+  def page_params
+    params.require(:page).permit(:name, :position, :visible, :permalink, :subject_id)
+  end
 
 end
