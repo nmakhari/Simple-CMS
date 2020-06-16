eb18734a375bcc0784dbef8abf363ac47e7b65e8
nmakhari <nmakhari@uwaterloo.ca>
2020-06-15 20:29:23 -0400

    Added username display on login page

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -5,7 +5,7 @@ class AccessController < ApplicationController
   before_action :confirm_logged_in, :except => [:login, :logout, :attempt_login]
 
   def menu
-    #display shit
+    @username = session[:username]
   end
 
   def login
@@ -20,10 +20,12 @@ class AccessController < ApplicationController
 
     if authenticated
       session[:user_id] = user.id
+      session[:username] = user.username
       flash[:notice] = "Successfully logged in"
       redirect_to(admin_path)
     else
       session[:user_id] = nil
+      session[:username] = nil
       flash.now[:notice] = "Incorrect Username/Password combination"
       render('login')
     end
